3fff8d53d20d8f7e5549640e5dca90cc0fb5c710
Dmitriy D. Volkov <dmitriy.v@group-ib.com>
2019-12-02 23:56:14 +0300

    removed some useless code, minor fixes

--- 2task/main.py
+++ 2task/main.py
@@ -6,11 +6,15 @@ from matplotlib import pyplot as plt
 grid_power = 1000
 
 
-def one_parameter_analisys():
+def one_parameter_analysis():
+    """
+    One-parameter analysis
+    The analysis is performed by parameter k2
+    """
     k1 = 1
     k_1 = 0.01
     k3 = 0.0032
-    k_3 = 2
+    k_3 = 0.002
 
     x = np.linspace(0, 0.999, grid_power)
     y = np.zeros(grid_power)
@@ -20,17 +24,7 @@ def one_parameter_analisys():
     jac = np.zeros(grid_power)
     D = np.zeros(grid_power)
 
-    y_hopf = []
-    x_hopf = []
-    k2_hopf = []
-
-    y_saddle_node = []
-    x_saddle_node = []
-    k2_saddle_node = []
-
-    y_node = []
-    x_node = []
-    k2_node = []
+    fig_1 = plt.figure()
 
     for i in range(1, grid_power):
         y[i] = k3 * x[i] / k_3
@@ -46,32 +40,17 @@ def one_parameter_analisys():
         D[i] = s[i] ** 2 - 4 * jac[i]
 
         if i == 1:
-            fig_1 = plt.figure()
             continue
 
+        # Hopf bifurcation
         if s[i] * s[i - 1] <= 0:
-            y_hopf.append(y[i])
-            x_hopf.append(x[i])
-            k2_hopf.append(k2[i])
-
             plt.plot(k2[i], x[i], 'go', label='hopf')
             plt.plot(k2[i], y[i], 'go', label='hopf')
 
+        # Saddle-node bifurcation
         if jac[i] * jac[i - 1] <= 0:
-            y_saddle_node.append(y[i])
-            x_saddle_node.append(x[i])
-            k2_saddle_node.append(k2[i])
-
             plt.plot(k2[i], x[i], 'r*', label='saddle-node')
             plt.plot(k2[i], y[i], 'r*', label='saddle-node')
-
-        if D[i] * D[i - 1] <= 0:
-            y_node.append(y[i])
-            x_node.append(x[i])
-            k2_node.append(k2[i])
-
-            plt.plot(k2[i], x[i], 'b^', label='node')
-            plt.plot(k2[i], y[i], 'b^', label='node')
 
     x_plot = fig_1.add_subplot(111)
     x_plot.plot(k2, x, label='k2(x)')
@@ -80,8 +59,16 @@ def one_parameter_analisys():
     y_plot.plot(k2, y, label='k2(y)')
 
     plt.title("One-parameter analisys")
+    plt.xlabel('k2')
+    plt.ylabel('x, y')
+    #plt.legend()
+
     plt.show()
 
 
+def two_parameter_analysis():
+    pass
+
+
 if __name__ == "__main__":
-    one_parameter_analisys()
+    one_parameter_analysis()
