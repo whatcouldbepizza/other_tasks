827ec2ad6fff06170206ae14da07f0c2d0c53e21
Dmitriy D. Volkov <dmitriy.v@group-ib.com>
2019-12-04 11:58:33 +0300

    switched to symbols

--- 2task/main.py
+++ 2task/main.py
@@ -1,67 +1,69 @@
 import numpy as np
+from sympy import solve, symbols, Matrix, re, trace
 
 from matplotlib import pyplot as plt
 
+from copy import deepcopy
 
-grid_power = 1000
+
+x, y, k1, k_1, k2, k3, k_3 = symbols("x y k1 k_1 k2 k3 k_3")
 
 
-def one_parameter_analysis():
+def one_parameter_analysis(f1, f2, param_values):
     """
     One-parameter analysis
     The analysis is performed by parameter k2
     """
-    k1 = 1
-    k_1 = 0.01
-    k3 = 0.0032
-    k_3 = 0.002
+    fig = plt.figure()
 
-    x = np.linspace(0, 0.999, grid_power)
-    y = np.zeros(grid_power)
-    k2 = np.zeros(grid_power)
+    jac_A = Matrix([f1, f2]).jacobian(Matrix([x, y]))
 
-    s = np.zeros(grid_power)
-    jac = np.zeros(grid_power)
-    D = np.zeros(grid_power)
+    param_dict = deepcopy(param_values)
+    del param_dict[k2]
 
-    fig_1 = plt.figure()
+    f1_subs = f1.subs([ (key, value) for key, value in param_dict.items() ])
+    f2_subs = f2.subs([ (key, value) for key, value in param_dict.items() ])
 
-    for i in range(1, grid_power):
-        y[i] = k3 * x[i] / k_3
-        k2[i] = (k1 * (1 - x[i] - y[i]) - k_1 * x[i] - k3 * x[i] + k_3 * y[i]) / (x[i] * ((1 - x[i] - y[i]) ** 2))
+    solution = solve([f1_subs, f2_subs], x, k2, dict=True)[0]
 
-        a11 = - k1 - k_1 - k3 - k2[i] * ((1 - x[i] - y[i]) ** 2) + 2 * x[i] * k2[i] * (1 - x[i] - y[i])
-        a12 = - k1 + k_3 + 2 * x[i] * k2[i] * (1 - x[i] - y[i])
-        a21 = k3
-        a22 = - k_3
+    y_list = np.linspace(0, 0.999, 1000)
 
-        s[i] = a11 + a22
-        jac[i] = a11 * a22 - a12 * a21
-        D[i] = s[i] ** 2 - 4 * jac[i]
+    x_list = []
+    k2_list = []
 
-        if i == 1:
-            continue
+    for val in y_list:
+        x_list.append(solution[x].subs({ y: val }))
+        k2_list.append(solution[k2].subs({ y: val }))
 
-        # Hopf bifurcation
-        if s[i] * s[i - 1] <= 0:
-            plt.plot(k2[i], x[i], 'go', label='hopf')
-            plt.plot(k2[i], y[i], 'go', label='hopf')
+    jac_subs = jac_A.subs([ (key, value) for key, value in param_dict.items() ])
+    jac_subs_listed = [ jac_subs.subs({ y: val, x: x_list[i], k2: k2_list[i] }) for i, val in enumerate(y_list) ]
 
-        # Saddle-node bifurcation
-        if jac[i] * jac[i - 1] <= 0:
-            plt.plot(k2[i], x[i], 'r*', label='saddle-node')
-            plt.plot(k2[i], y[i], 'r*', label='saddle-node')
+    det_list = []
+    trace_list = []
 
-    x_plot = fig_1.add_subplot(111)
-    x_plot.plot(k2, x, label='k2(x)')
+    for jac in jac_subs_listed:
+        det_list.append(jac.det())
+        trace_list.append(trace(jac))
 
-    y_plot = fig_1.add_subplot(111)
-    y_plot.plot(k2, y, label='k2(y)')
+    for i in range(2, len(det_list)):
+        try:
+            if det_list[i] * det_list[i - 1] <= 0:
+                plt.plot(k2_list[i], x_list[i], 'r*', label='saddle-node')
+                plt.plot(k2_list[i], y_list[i], 'r*', label='saddle-node')
+        except Exception:
+            print(det_list[i])
+            print(det_list[i - 1])
+            raise
 
-    plt.title("One-parameter analisys")
+        if trace_list[i] * trace_list[i - 1] <= 0:
+            plt.plot(k2_list[i], x_list[i], 'go', label='hopf')
+            plt.plot(k2_list[i], y_list[i], 'r*', label='saddle-node')
+
+    plt.plot(k2_list, x_list, linewidth=1.5, label='k2(x)')
+    plt.plot(k2_list, y_list, linestyle='--', linewidth=1.5, label='k2(y)')
+
     plt.xlabel('k2')
     plt.ylabel('x, y')
-    #plt.legend()
 
     plt.show()
 
@@ -71,4 +73,9 @@ def two_parameter_analysis():
 
 
 if __name__ == "__main__":
-    one_parameter_analysis()
+    param_values = { k1: 1, k_1: 0.01, k2: 2, k3: 0.0032, k_3: 0.002 }
+
+    dxdt = k1 * (1 - x - y) - k_1 * x - k3 * x + k_3 * y - k2 * ((1 - x - y) ** 2) * x
+    dydt = k3 * x - k_3 * y
+
+    one_parameter_analysis(dxdt, dydt, param_values)
